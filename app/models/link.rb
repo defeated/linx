310b3e2c415ede17652874a879a14ff1a1b820f2
eddie cianci <defeated2k4@gmail.com>
2012-09-02 17:27:49 -0400

    add Link#for_date scope

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,18 @@
 class Link < ActiveRecord::Base
   validates :url, format: URI::regexp(%w(http https))
   attr_accessible :url
+
+  def self.for_date(year, month = nil, day = nil)
+    time = Time.local year, month, day
+
+    range = if day
+              (time.beginning_of_day)..(time.end_of_day)
+            elsif month
+              (time.beginning_of_month)..(time.end_of_month)
+            elsif year
+              (time.beginning_of_year)..(time.end_of_year)
+            end
+
+    where created_at: range
+  end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -3,8 +3,31 @@ require 'spec_helper'
 describe Link do
   fixtures :links
 
-  it { should_not be_valid }
-  it "validates url" do
-    links(:google).should be_valid
+  let(:google)    { links(:google)    }
+  let(:oldest)    { links(:oldest)    }
+  let(:halloween) { links(:halloween) }
+
+  context "validations" do
+    it { should_not be_valid }
+    it "validates url" do
+      links(:google).should be_valid
+    end
+  end
+
+  context "scopes" do
+    it "finds for year" do
+      Link.for_date(2012).all.should include(google)
+      Link.for_date(2012).all.should_not include(oldest)
+    end
+
+    it "finds for month" do
+      Link.for_date(2012, 10).all.should include(halloween)
+      Link.for_date(2012, 10).all.should_not include(oldest)
+    end
+
+    it "finds for day" do
+      Link.for_date(2012, 10, 31).all.should include(halloween)
+      Link.for_date(2012, 10, 31).all.should_not include(oldest)
+    end
   end
 end
