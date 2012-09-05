fba6465dd6bc192c2c86fb205704ecea99aaf5be
eddie cianci <defeated2k4@gmail.com>
2012-09-04 22:37:14 -0400

    add Link#source column

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120901112708) do
+ActiveRecord::Schema.define(:version => 20120905022859) do
 
   create_table "links", :force => true do |t|
     t.string   "url",        :null => false
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "source"
   end
 
 end
