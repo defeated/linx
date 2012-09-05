b65cc9710dfbea723ed0df1b9d7afffd193ee539
eddie cianci <defeated2k4@gmail.com>
2012-09-04 21:45:56 -0400

    add default root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Linx::Application.routes.draw do
   resources :links, only: [:index, :show, :create]
 
-  match "/auth/:provider/callback", to: "sessions#create", as: "auth_callback"
+  match "/auth/:provider/callback", to: "sessions#create"
 
+  root to: "links#index"
 end
--- spec/routing/custom_routes_spec.rb
+++ spec/routing/custom_routes_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe "custom routes" do
+  it "defaults root to links#index" do
+    get(root_path).should route_to("links#index")
   end
 end
