5f3036cbf61ce104207d036b20ee0ef3ab687153
eddie cianci <defeated2k4@gmail.com>
2012-09-04 21:45:45 -0400

    remove /year/month/day routes, feels wrong.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,4 @@ Linx::Application.routes.draw do
 
   match "/auth/:provider/callback", to: "sessions#create", as: "auth_callback"
 
-  match "/:year(/:month(/:day))", to: "links#index", constraints: {
-    year: /\d{4}/, month: /\d{1,2}/, day: /\d{1,2}/
-  }
 end
--- spec/routing/custom_routes_spec.rb
+++ spec/routing/custom_routes_spec.rb
@@ -1,20 +1,5 @@
 require 'spec_helper'
 
 describe "custom routes" do
-  it "routes years" do
-    get("/2012").should route_to("links#index", year: "2012")
-  end
-
-  it "routes months" do
-    get("/2012/10").should route_to("links#index", year: "2012", month: "10")
-  end
-
-  it "routes years" do
-    get("/2012/10/31").should route_to("links#index", year: "2012",
-      month: "10", day: "31")
-  end
-
-  it "doesn't route nonsense" do
-    get("/yyyy/mm/dd").should_not be_routable
   end
 end
